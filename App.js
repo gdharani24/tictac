24db219eb025d8f124106fcf25a67fc2045ea812
gdharani24 <gdharani24@gmail.com>
2024-12-31 15:59:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tictactoe from './components/Tictactoe/Tictactoe';
+
+function App() {
+  return (
+    <div>
+   <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
